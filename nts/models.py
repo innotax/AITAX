aa942c55dc36a37f7abd36dae7a4e8695b0fa12d
taxkmj <taxkmj@naver.com>
2019-12-30 00:15:33 +0900

    191230 새벽

--- nts/models.py
+++ nts/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 class CtaCert(models.Model):
+    
     cert_nm = models.CharField(max_length=100, verbose_name='인증서명')
-    end_dt = models.DateField(verbose_name='유효기간')
+    pub_dt = models.DateField(verbose_name='유효기간시작일')
+    end_dt = models.DateField(verbose_name='유효기간종료일')
     org_nm = models.CharField(max_length=20, verbose_name='인증기관')
+    oid = models.CharField(max_length=30, verbose_name='인증기관ID')
     sn = models.CharField(max_length=20, verbose_name='시리얼넘버')
     file1 = models.CharField(max_length=1000, verbose_name='인증서')
     file2 = models.CharField(max_length=1000, verbose_name='개인키')
     cert_pw = models.CharField(max_length=20, verbose_name='인증서비번')
     
+    class Meta:
+        unique_together = (('cert_nm', 'end_dt'),)
--- nts/views.py
+++ nts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.conf import settings
+from utils import ift
+import json
+from nts.models import CtaCert
+from django.db import IntegrityError
+from django.contrib import messages  # 경고창 https://hoy.kr/kxuTa
 
 # Create your views here.
 
@@ -8,5 +13,18 @@ def cert_info(request):
     return render(request, 'cert_info.html')
 
 def output(request):
-    data = "test data"
-    return render(request, 'cert_info.html', {'data':data})
+    data = ift.cert_info()
+    dic = json.loads(data)
+    if dic['cert_nm'] == "":
+        msg = "공인인증서 선택 및 비밀번호 입력후 확인버튼을 누르세요!!!"
+        messages.add_message(request, messages.INFO, msg)
+        return render(request, 'cert_info.html')
+    try:
+        ctacert = CtaCert(**dic)  # 인스턴스
+        ctacert.save()
+    except IntegrityError as e:
+        msg = "이미 저장된 공인인증서 입니다!!!"
+        messages.add_message(request, messages.INFO, msg)
+        return render(request, 'cert_info.html')
+        
+    return render(request, 'cert_info.html', {'dic':dic})
